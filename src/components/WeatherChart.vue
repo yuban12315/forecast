4e5091751842f407507c993e5bff83bb94d399ab
yuban12315 <fengyulin@imudges.com>
2018-08-14 15:46:26 +0800

    :tada: 添加24小时天气图表

--- src/components/WeatherChart.vue
+++ src/components/WeatherChart.vue
@@ -8,7 +8,9 @@
     export default {
         name: "WeatherChart",
         data(){
-            weatherDataList
+            return{
+                weatherDataList
+            }
         }
     }
 </script>
