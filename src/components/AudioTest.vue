9c7c99c9aab7c98cef9a9c356210fb5503db5cd1
BoyGK <1215356195@qq.com>
2018-08-17 09:13:18 +0800

    view

---
+++ src/components/AudioTest.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <audio id="vid"  autoplay loop controls>
+        </audio>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "audio-test",
+        methods: {
+            async getAudio(city) {
+                const url = `${city}`
+                const res = await this.$axios.post(`/testApi/audio.action`, {url})
+                console.log(res.data)
+
+                var vid = document.getElementById('vid')
+                vid.src = res.data
+                vid.load()
+                vid.play
+            }
+        },
+        async created() {
+           await this.getAudio('beijing')
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
